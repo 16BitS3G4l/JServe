1021b236a8f1aec986727ccae262bbabb304d343
dev-segal <ysegal7@student.touro.edu>
2020-11-30 09:22:23 -0500

    Modifying signature to comply with ResponseStatusNullException

--- lib/src/main/java/com/devsegal/jserve/Response.java
+++ lib/src/main/java/com/devsegal/jserve/Response.java
@@ -1,6 +1,6 @@
 package com.devsegal.jserve;
 
 interface Response {	
-	public void setResponseHeaders(ResponseHeaders responseHeaders);
+	public void setResponseHeaders(ResponseHeaders responseHeaders) throws ResponseStatusNullException;
 	public void insertContent(String content);
 }
