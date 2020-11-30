ea2111b1e3ec50b96ce1d6d73044b85469fe371d
dev-segal <ysegal7@student.touro.edu>
2020-11-30 09:21:44 -0500

    To be raised in situations when dealing with null response status's.

---
+++ lib/src/main/java/com/devsegal/jserve/ResponseStatusNullException.java
@@ -0,0 +1,11 @@
+package com.devsegal.jserve;
+
+public class ResponseStatusNullException extends Exception {
+        public ResponseStatusNullException() {
+            super();
+        }
+
+        public ResponseStatusNullException(String message) {
+            super(message);
+        }
+}
