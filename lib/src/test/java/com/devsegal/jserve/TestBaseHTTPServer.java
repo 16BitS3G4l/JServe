1b71714f5179082f47302a6aedb7690acb2e550b
dev-segal <ysegal7@student.touro.edu>
2020-12-29 14:32:25 -0500

    Increasing test suite scope

---
+++ lib/src/test/java/com/devsegal/jserve/TestBaseHTTPServer.java
@@ -0,0 +1,11 @@
+package com.devsegal.jserve;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class TestBaseHTTPServer {
+    
+}
